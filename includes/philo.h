91e113ecd6e53f952c8db4f87aef3e7d25fd8ec1
Rui Ribeiro <42305006+ruiribeiro04@users.noreply.github.com>
2025-10-03 13:22:18 +0100

    Refactor data structures and enhance argument validation in the philosopher simulation
    - Added basic routine
    - Added needed struct entries
    - Added test dynamic thread creation

--- includes/philo.h
+++ includes/philo.h
@@ -1,19 +1,31 @@
 #ifndef PHILO_H
 #define PHILO_H
 
-#include <unistd.h>
+#include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
 
 	// number_of_philosophers time_to_die time_to_eat
 	// time_to_sleep [number_of_times_each_philosopher_must_eat]
 
-typedef struct	s_philo
+typedef struct	s_data
 {
 	int n_philo;
 	int time_die;
 	int time_eat;
 	int time_sleep;
 	int n_times_eat;
+}	t_data;
+
+typedef struct	s_philo
+{
+	int id;
+	int	alive;
+	int	left_fork;
+	int	right_fork;
+	pthread_t	thread;
+	struct s_data *data;
 }	t_philo;
 
 
@@ -23,6 +35,9 @@ int	ft_atoi(const char *str);
 
 // data.c
 int check_args(int ac, char **av);
-void	init_data(t_philo *data, int ac, char **av);
+void	init_data(t_data *data, int ac, char **av);
+
+// philo threads
+void *philo_routine(void *arg);
 
 #endif
--- src/data.c
+++ src/data.c
@@ -1,5 +1,21 @@
 #include "philo.h"
 
+static int	is_numeric(const char *s)
+{
+	int i;
+
+	if (!s || s[0] == '\0')
+		return (0);
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
 int check_args(int ac, char **av)
 {
 	// number_of_philosophers time_to_die time_to_eat
@@ -10,6 +26,10 @@ int check_args(int ac, char **av)
 		ft_putstr_fd("time_to_die time_to_eat time_to_sleep ", 1);
 		return (ft_putstr_fd("[number_of_times_each_philosopher_must_eat]\n", 1), -1);
 	}
+	/* Require arguments to be strictly numeric (no trailing letters like "10a") */
+	if (!is_numeric(av[1]) || !is_numeric(av[2]) || !is_numeric(av[3])
+			|| !is_numeric(av[4]) || (ac == 6 && !is_numeric(av[5])))
+		return (ft_putstr_fd("Error: All arguments must be positive integers\n", 2), -1);
 	if (ft_atoi(av[1]) <= 0 || ft_atoi(av[1]) > 200)
 		return (ft_putstr_fd("Error: Numbers of philos is invalid!\n", 2), -1);
 	if (ft_atoi(av[2]) < 0 || ft_atoi(av[3]) < 0 || ft_atoi(av[4]) < 0)
@@ -20,7 +40,7 @@ int check_args(int ac, char **av)
 	return (1);
 }
 
-void	init_data(t_philo *data, int ac, char **av)
+void	init_data(t_data *data, int ac, char **av)
 {
 	data->n_philo = ft_atoi(av[1]);
 	data->time_die = ft_atoi(av[2]);
